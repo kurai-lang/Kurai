5bbf29a763d832d4c037c19fb44db835939ea868
Zairy Putra <zairysbigtae@gmail.com>
2025-07-09 11:07:47 +0800

    *screams*

--- kuraic/src/bin/test.rs
+++ kuraic/src/bin/test.rs
@@ -27,7 +27,7 @@ fn main() {
     // "#.to_string();
 
     let code = r#"
-        extern fn free_list_allocator_allocate(size: i64) *u8;
+        // extern fn free_list_allocator_allocate(size: i64) *u8;
 
         // fn test() void {
         //     printf("hello");
